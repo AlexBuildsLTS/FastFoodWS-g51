5b8c1562fe744311e6b5f224a19177afdde06a15
Alex Youssef <84400592+AlexBuildsLTS@users.noreply.github.com>
2024-11-23 00:26:25 +0200

    Add core application structure with App and main

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import { CartProvider } from './context/CartContext';
+import { ThemeProvider } from './context/ThemeContext';
+import Navbar from './components/Navbar';
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <CartProvider>
+        <Router>
+          <div className="min-h-screen transition-colors bg-gray-50 dark:bg-navy-900">
+            <Navbar />
+            <main className="pt-20 pb-12">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/menu" element={<Menu />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/checkout" element={<Checkout />} />
+              </Routes>
+            </main>
+          </div>
+        </Router>
+      </CartProvider>
+    </ThemeProvider>
+  );
+}
